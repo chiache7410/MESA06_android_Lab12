565fd608857de5ae987e07875d339f3c547c65e3
chiache <chiache7410@gmail.com>
2016-11-23 16:39:49 +0800

    2016-11-23_test8

--- app/src/main/java/tw/org/iii/lab12/MainActivity.java
+++ app/src/main/java/tw/org/iii/lab12/MainActivity.java
@@ -1,9 +1,16 @@
 package tw.org.iii.lab12;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,7 +19,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -27,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     private UIHandler uiHandler;
     private ImageView imageView;
     private Bitmap bmp;
+    private String urlDownload = "http://www.iii.org.tw";
+    private File sdroot;
+    private ProgressDialog pDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +47,33 @@ public class MainActivity extends AppCompatActivity {
         uiHandler = new UIHandler();
         textView = (TextView)findViewById(R.id.tv);
         imageView = (ImageView)findViewById(R.id.img);
+        // 1.權限
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //request permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    0);
+        }
+        // 2.環境
+        init();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        init();
+    }
+
+    private void init(){
+        sdroot = Environment.getExternalStorageDirectory();
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Download...");
+        pDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+    }
+    //UDP Sender
     public void test1(View v) {
         new Thread(){
             @Override
@@ -148,6 +187,53 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
     }
+    // Download save to SDCard
+    public void test7(View v){
+        pDialog.show();
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://pdfmyurl.com/?url=" + urlDownload);
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    conn.connect();
+                    InputStream in = conn.getInputStream();
+                    File download = new File(sdroot, "brad.pdf");
+                    FileOutputStream fout = new FileOutputStream(download);
+                    byte[] buf = new byte[4096];
+                    int len;
+                    while ( (len = in.read(buf)) != -1 ){
+                        fout.write(buf,0,len);
+                    }
+                    fout.flush();
+                    fout.close();
+                    Log.v("brad", "Download OK");
+                    uiHandler.sendEmptyMessage(3);
+                }catch (Exception e){
+                    Log.v("brad", "Download :" + e.toString());
+                    uiHandler.sendEmptyMessage(3);
+                }
+            }
+        }.start();
+    }
+    // Upload to My Server => add2.php
+    public void test8(View v){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    MultipartUtility mu = new MultipartUtility("http://10.1.6.58/add2.php", "UTF-8");
+                    mu.addFormField("account", "mary");
+                    mu.addFormField("passwd", "654321");
+                    mu.addFilePart("upload", new File(sdroot, "brad.pdf"));
+                    List<String> ret = mu.finish();
+                    Log.v("brad", ret.get(0));
+                }catch (Exception e){
+                    Log.v("brad", e.toString());
+                }
+            }
+        }.start();
+    }
     private class UIHandler extends Handler {
         @Override
         public void handleMessage(Message msg) {
@@ -160,6 +246,9 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 case 2:
                     imageView.setImageBitmap(bmp);
+                    break;
+                case 3:
+                    pDialog.dismiss();
                     break;
             }
         }
